d9e3fc8e597a39d9b5b6c4f4e4bac284ee4d495e
Anton Punith <antonpunith@gmail.com>
2019-12-08 23:07:39 +1100

    adding chapter 06

---
+++ Chapter 06/Exercise 6.01/src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+// styles
+import "./App.css";
+
+function App() {
+  return <div className="container center">6.01</div>;
+}
+
+export default App;
---
+++ Chapter 06/Exercise 6.02/src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+// styles
+import "./App.css";
+
+function App() {
+  return <div className="container center">6.02</div>;
+}
+
+export default App;

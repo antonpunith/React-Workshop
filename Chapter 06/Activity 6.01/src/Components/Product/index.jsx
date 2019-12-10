2eef4fb708f33bde388693037bf211348b904634
Anton Punith <antonpunith@gmail.com>
2019-12-10 22:03:54 +1100

    conditionally show description

--- Chapter 06/Activity 6.01/src/Components/Product/index.jsx
+++ Chapter 06/Activity 6.01/src/Components/Product/index.jsx
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { Component } from "react";
 
 import "./product.css";
 
-const Product = props => (
-  <div className="product">
-    <h2>{props.product.name}</h2>
-  </div>
-);
+class Product extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showDescription: false
+    };
+    this.toggleDescription = this.toggleDescription.bind(this);
+  }
+  toggleDescription() {
+    this.setState({ showDescription: !this.state.showDescription });
+  }
+  render() {
+    const { name, price, summary } = this.props.product;
+    const { showDescription } = this.state;
+    return (
+      <div className="product">
+        <h2>{name}</h2>
+        <strong>${price}</strong>
+        <button onClick={this.toggleDescription}>
+          {showDescription ? "-" : "+"}
+        </button>
+        {showDescription && <p>{summary}</p>}
+      </div>
+    );
+  }
+}
 
 export { Product };
